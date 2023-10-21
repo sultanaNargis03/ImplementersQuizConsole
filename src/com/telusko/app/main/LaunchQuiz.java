30f20717c0da9237cb1dd2b6434418e06671c7e5
Nargis Sultana <85351131+sultanaNargis03@users.noreply.github.com>
2023-10-21 16:30:55 +0530

    Update LaunchQuiz.java
    "given extra chances in case of wrong user input"

--- src/com/telusko/app/main/LaunchQuiz.java
+++ src/com/telusko/app/main/LaunchQuiz.java
@@ -20,6 +20,8 @@ public class LaunchQuiz
 		System.out.println("***************************************");
 		System.out.println();
 		boolean valid=false;
+		boolean valid2=false;
+		boolean valid3=false;
 		
 		do {
 			System.out.println("Are you ready to play(Y/N)?");
@@ -28,21 +30,35 @@ public class LaunchQuiz
 			String input = sc.nextLine();
 			if (input.equalsIgnoreCase("Y")) 
 			{
+				do {
 				System.out.println("Please select your Quiz topic\nJava\nSpring Boot\nC");
 				String topic=sc.nextLine();
-				System.out.println("Please select the Quiz level\nBasic\nIntermediate\nAdvanced");
-				String level=sc.nextLine();
-				if((topic.equalsIgnoreCase("Java")||topic.equalsIgnoreCase("Spring Boot")||topic.equalsIgnoreCase("C"))&&
-						((level.equalsIgnoreCase("Basic")||level.equalsIgnoreCase("Intermediate")||level.equalsIgnoreCase("Advanced"))))
+				
+				if((topic.equalsIgnoreCase("Java")||topic.equalsIgnoreCase("Spring Boot")||topic.equalsIgnoreCase("C")))		
 				{	
-					QuizService service = new QuizService();
-					service.playQuiz(topic,level);	
-					System.out.println("Thank you for playing!!");
+					do {
+					System.out.println("Please select the Quiz level\nBasic\nIntermediate\nAdvanced");
+					String level=sc.nextLine();
+					if((level.equalsIgnoreCase("Basic")||level.equalsIgnoreCase("Intermediate")||level.equalsIgnoreCase("Advanced")))
+					{
+						QuizService service = new QuizService();
+						service.playQuiz(topic,level);	
+						System.out.println("Thank you for playing!!");
+						valid3=true;
+					}
+					else
+					{
+						System.out.println("please choose from the listed!!");
+					}
+					}while(!valid3);
+					valid2=true;
 				}
 				else
 				{
-					System.out.println("Please choose from the listed one");
+					System.out.println("invalid input!!");
 				}
+				}while(!valid2);
+				
 				valid=true;
 			}
 			
@@ -56,7 +72,6 @@ public class LaunchQuiz
 				System.out.println("Please enter a correct input");
 			}
 		}while(!valid);
-			
-			
+					
 	}
 }
