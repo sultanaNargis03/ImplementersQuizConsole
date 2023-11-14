e1661287f44e34084db21e4387cbf8ab2823b5ed
sultanaNargis03 <nargissultana969@gmail.com>
2023-11-14 14:41:25 +0530

    "java 8 features"

--- src/com/telusko/app/service/QuizService.java
+++ src/com/telusko/app/service/QuizService.java
@@ -1,41 +1,37 @@
 package com.telusko.app.service;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import com.telusko.app.dao.Question;
 
 
 public class QuizService 
 {
-	Question questions[] =new Question[18];
+	ArrayList<Question>questions=new ArrayList<>();
 	static int score=0;
 	
 	public QuizService()
 	{
-		questions[0] = new Question(1, "Java", "size of int","Easy", "2", "6", "4", "8", "4");
-		questions[1] = new Question(2, "Java", "size of double","Easy" ,"2", "6", "4", "8", "8");
-		questions[2] = new Question(3, "Java", "default capacity of StringBuffer/StringBuilder class","Hard", "0", "16", "34", "18", "16");
-		questions[3] = new Question(4, "Java", "In java Array is treated as an- ", "Hard","class", "object", "method", "variable", "object");
-		questions[4] = new Question(5, "Java", "this keyword refered as","medium", "current object", "parent class", "main method", "final variable", "current object");
-		questions[5] = new Question(6, "Java", "in java object created on","medium", "heap", "stack", "method area", "constructor", "heap");
 		
-		questions[6] = new Question(1, "Spring Boot", "Which Spring annotation is used to handle HTTP PUT requests?","Medium",
-				"@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@PutMapping");
-		questions[7] = new Question(2, "Spring Boot", "Which Spring annotation is used to handle HTTP POST requests?","Easy",
-				"@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@PostMapping");
-		questions[8] = new Question(3, "Spring Boot", "Which Spring annotation is used to handle HTTP DELETE requests?","Easy",
-				"@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@DeleteMapping");
-		questions[9] = new Question(4, "Spring Boot", "Which Spring annotation is used to handle HTTP GET requests?","Medium",
-				"@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@GetMapping");
-		questions[10] = new Question(5, "Spring Boot",
-				"Which annotation do we use to mark the class as a Service class/component?","Hard", "@Service", "@Controller",
-				"@Repository", "@Component", "@Service");
-		questions[11] = new Question(5, "Spring Boot",
-				"Which starter dependency is used to develop web applications or Restful web services\nspring-boot-starter-data-??","Hard", "@jpa", "@web",
-				"@rest", "@None", "@web");
+		questions.add(new Question(1, "Java", "size of int","Basic", "2", "6", "4", "8", "4"));
+		questions.add(new Question(2, "Java", "size of double","Basic" ,"2", "6", "4", "8", "8"));
+		questions.add(new Question(3, "Java", "default capacity of StringBuffer/StringBuilder class","Advanced", "0", "16", "34", "18", "16"));
+		questions.add(new Question(4, "Java", "In java Array is treated as an- ", "Advanced","class", "object", "method", "variable", "object"));
+		questions.add(new Question(5, "Java", "this keyword refered as","Intermediate", "current object", "parent class", "main method", "final variable", "current object"));
+		questions.add(new Question(6, "Java", "in java object created on","Intermediate", "heap", "stack", "method area", "constructor", "heap"));
 		
+		questions.add(new Question(1, "Spring Boot", "Which Spring annotation is used to handle HTTP PUT requests?","Intermediate","@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@PutMapping"));
+		questions.add(new Question(2, "Spring Boot", "Which Spring annotation is used to handle HTTP POST requests?","Basic","@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@PostMapping"));
+		questions.add(new Question(3, "Spring Boot", "Which Spring annotation is used to handle HTTP DELETE requests?","Basic","@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@DeleteMapping"));
+		questions.add(new Question(4, "Spring Boot", "Which Spring annotation is used to handle HTTP GET requests?","Intermediate","@GetMapping", "@PutMapping", "@PostMapping", "@DeleteMapping", "@GetMapping"));
+		questions.add(new Question(5, "Spring Boot","Which annotation do we use to mark the class as a Service class/component?","Advanced", "@Service", "@Controller","@Repository", "@Component", "@Service"));
+		questions.add(new Question(6, "Spring Boot","Which starter dependency is used to develop web applications or Restful web services\nspring-boot-starter-data-??","Advanced", "@jpa", "@web","@rest", "@None", "@web"));
 		
-		questions[12] = new Question(1, "C", "What will be the output of the following code snippet?\r\n"
+		questions.add(new Question(1, "C", "What will be the output of the following code snippet?\r\n"
 				+ "\r\n"
 				+ "#include <stdio.h>\r\n"
 				+ "int main() {\r\n"
@@ -45,15 +41,15 @@ public class QuizService
 				+ "	b = t;\r\n"
 				+ "	printf(\"%d %d\", a, b);\r\n"
 				+ "	return 0;\r\n"
-				+ "}", "3 5","Medium","3 3", "5 3", "5 5", "5 3");
-		questions[13] = new Question(2, "C", "What is the output of the following code snippet?\r\n"
+				+ "}", "3 5","Intermediate","3 3", "5 3", "5 5", "5 3"));
+		questions.add(new Question(2, "C", "What is the output of the following code snippet?\r\n"
 				+ "\r\n"
 				+ "int main() {\r\n"
 				+ "	int sum = 2 + 4 / 2 + 6 * 2;\r\n"
 				+ "	printf(\"%d\", sum);\r\n"
 				+ "	return 0;\r\n"
-				+ "}","Hard", "2", "15", "16", "18", "16");
-		questions[14] = new Question(3, "C", "What is the output of the following code snippet?\r\n"
+				+ "}","Advanced", "2", "15", "16", "18", "16"));
+		questions.add(new Question(3, "C", "What is the output of the following code snippet?\r\n"
 				+ "\r\n"
 				+ "#include <stdio.h>\r\n"
 				+ "int main() {\r\n"
@@ -63,8 +59,8 @@ public class QuizService
 				+ "	    sum += a[i];\r\n"
 				+ "	}\r\n"
 				+ "	printf(\"%d\", sum);\r\n"
-				+ "	return 0;","Easy", "1", "4", "20", "10", "10");
-		questions[15] = new Question(4, "C", " \r\n"
+				+ "	return 0;","Basic", "1", "4", "20", "10", "10"));
+		questions.add(new Question(4, "C", " \r\n"
 				+ "What will be the output of the following code snippet?\r\n"
 				+ "\r\n"
 				+ "#include <stdio.h>\r\n"
@@ -76,8 +72,8 @@ public class QuizService
 				+ "int main() {\r\n"
 				+ "	solve();\r\n"
 				+ "	return 0;\r\n"
-				+ "}", "Hard","12", "24", "20", "18", "20");
-		questions[16] = new Question(5, "C", " \r\n"
+				+ "}", "Advanced","12", "24", "20", "18", "20"));
+		questions.add(new Question(5, "C", " \r\n"
 				+ "What will be the output of the following code snippet?\r\n"
 				+ "\r\n"
 				+ "#include <stdio.h>\r\n"
@@ -89,9 +85,9 @@ public class QuizService
 				+ "int main() {\r\n"
 				+ "	solve();\r\n"
 				+ "	return 0;\r\n"
-				+ "}","Medium", "104", "24", "10", "34", "34");
+				+ "}","Intermediate", "104", "24", "10", "34", "34"));
+		questions.add(new Question(6,"C"," What is an example of iteration in C?","Basic", "for", "while", "do-whiile", "All", "All"));
 		
-		questions[17] = new Question(5,"C"," What is an example of iteration in C?","Easy", "for", "while", "do-whiile", "All", "All");
 	}
 	public String userInput(Question q) throws NullPointerException
 	{
@@ -178,80 +174,34 @@ public class QuizService
 	public void playQuiz(String topic,String level) 
 	{
 		
-		
-		for(Question q:questions)
-		{
-			if(level.equalsIgnoreCase("Basic"))
-			{
-				if(topic.equalsIgnoreCase(q.getCategory())&&q.getDifficultyLevel().equalsIgnoreCase("Easy"))
-				{
-					System.out.println(q.getId() + ". " + q.getQuestionTitle());
-					System.out.println(
-							"A." + q.getOption1() + " B." + q.getOption2() + " C." + q.getOption3() + " D." + q.getOption4());
 
-					try {
+			List<Question> s1=
+					questions.stream()
+								.filter(i->i.getDifficultyLevel().equalsIgnoreCase(level))
+								.filter(j->j.getCategory().equalsIgnoreCase(topic))
+								.toList();
+								;
+								
+		    for(Question q:s1)
+		    {
+			   System.out.println(q.getId() + ". " + q.getQuestionTitle()+"\n"+"A." + q.getOption1() + " B." + q.getOption2() + " C." + q.getOption3() + " D." + q.getOption4());
+		    
+
+				try 
+				{
 					String userAnswer =userInput(q);
 					calculateScore(userAnswer,q);
-					}
-					catch(NullPointerException e)
-					{
-						System.out.println("no marks will be added for this question!!");
-					}
-					catch(Exception e)
-					{
-						System.out.println(e.getMessage());
-					}
-					
 				}
-			}
-			else if(level.equalsIgnoreCase("Intermediate"))
-			{
-				if(topic.equalsIgnoreCase(q.getCategory())&&q.getDifficultyLevel().equalsIgnoreCase("Medium"))
+				catch(NullPointerException e)
 				{
-					System.out.println(q.getId() + ". " + q.getQuestionTitle());
-					System.out.println(
-							"A." + q.getOption1() + " B." + q.getOption2() + " C." + q.getOption3() + " D." + q.getOption4());
-					try {
-						String userAnswer =userInput(q);
-						calculateScore(userAnswer,q);
-						}
-						catch(NullPointerException e)
-						{
-							System.out.println("sorry!! we can't accept your answer");
-						}
-						catch(Exception e)
-						{
-							System.out.println(e.getMessage());
-						}
+					System.out.println("no marks will be added for this question!!");
 				}
-			}
-			else if(level.equalsIgnoreCase("Advanced"))
-			{
-				if(topic.equalsIgnoreCase(q.getCategory())&&q.getDifficultyLevel().equalsIgnoreCase("Hard"))
+				catch(Exception e)
 				{
-					System.out.println(q.getId() + ". " + q.getQuestionTitle());
-					System.out.println(
-							"A." + q.getOption1() + " B." + q.getOption2() + " C." + q.getOption3() + " D." + q.getOption4());
-					try {
-						String userAnswer =userInput(q);
-						calculateScore(userAnswer,q);
-						}
-						catch(NullPointerException e)
-						{
-							System.out.println("sorry!! we can't accept your answer");
-						}
-						catch(Exception e)
-						{
-							System.out.println(e.getMessage());
-						}
+					System.out.println(e.getMessage());
 				}
-			}
-			else
-			{
-				System.out.println("You have entered a wrong input");
-			}
-		}	
-			System.out.println("score = " + score);
+		    }
+			System.out.println("score = " + score);			
 		
 
 	}
